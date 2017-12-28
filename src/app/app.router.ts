f304ef45f9d261bcfaa07d458614877e2228bd09
P@rmar117 <jollyprashil@gmail.com>
2017-12-28 10:21:57 +0530

    login done

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -4,11 +4,13 @@ import { TravelerComponent } from './traveler/traveler.component';     // Add th
 import { HotelComponent } from './hotel/hotel.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { UserComponent } from './user/user.component';
+import { LoginComponent } from './login/login.component';
 
 const routing:Routes = [
-    {path: '',redirectTo:'/user',pathMatch:'full'},
+    {path: '',redirectTo:'/login',pathMatch:'full'},
     { path: 'user',component: UserComponent},
     {path: 'traveler',component: TravelerComponent},
-    {path: 'hotels',component: HotelComponent}
+    {path: 'hotels',component: HotelComponent},
+    {path:'login',component:LoginComponent}
   ];
   export const routingArray=RouterModule.forRoot(routing);
