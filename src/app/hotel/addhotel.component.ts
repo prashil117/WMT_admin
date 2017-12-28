b66aaf2b8fe92e5d843371c37ff2a8cc66ffe3ab
P@rmar117 <jollyprashil@gmail.com>
2017-12-28 11:30:02 +0530

    done fsff

---
+++ src/app/hotel/addhotel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-addhotel',
+  templateUrl: './addhotel.component.html',
+  styleUrls: ['./addhotel.component.css']
+})
+export class AddhotelComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
