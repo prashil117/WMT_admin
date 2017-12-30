816ac42ddad6a92052b2a07bc39c3bc7917d16c4
keval718 <kevalvirat@gmail.com>
2017-12-30 10:47:00 +0530

    design done

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -6,10 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent implements OnInit {
+  email:string="";
 
   constructor() { }
 
   ngOnInit() {
+    this.email=localStorage.getItem('Email');
   }
 
 }
