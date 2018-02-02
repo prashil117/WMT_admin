433b21513d372b1bfca2fe4b500d709b483e1317
thakorraj <thakorraj80@yahoo.com>
2018-02-02 14:29:58 +0530

    Images done in all

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserserviceService } from '../user/userservice.service';
+import { User } from '../user/userc';
 
 @Component({
   selector: 'app-navbar',
@@ -7,11 +9,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NavbarComponent implements OnInit {
   email:string="";
+  name:string="";
+  img:string="";
 
-  constructor() { }
+  constructor(public _data:UserserviceService) { }
 
   ngOnInit() {
     this.email=localStorage.getItem('Email');
+
+    this._data.getUserById(this.email).subscribe(
+      (data:User[])=>{
+        this.name=data[0].user_name;
+        this.img=data[0].user_photo;
+      }
+    );
   }
 
 }
